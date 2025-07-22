08e3abf5862b38fdfe782eb48ebb0117959497ef
SrFelez <fele.es@gmail.com>
2025-07-22 20:54:18 +0200

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDV3yqRovhDQdOPUNmpS8vTJjDdbE9rieI",
+  authDomain: "trivial-canalla.firebaseapp.com",
+  projectId: "trivial-canalla",
+  storageBucket: "trivial-canalla.appspot.com",
+  messagingSenderId: "438977635010",
+  appId: "1:438977635010:web:8318b097ddc12b0fe4915b"
+};
+
